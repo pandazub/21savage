47dfaa8aada934bc48076d7296d7061945556bd5
pandazub <ustanley73@gmail.com>
2022-06-21 14:44:28 +0100

    init commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// window.addEventListener("load",function())
+let typed=new Typed(".Typing-effect",{
+    strings :[
+        "Branding",
+        "Business",
+        "Start Up",
+    ],
+    typeSpeed: "9",
+    loop: true,
+    backSpeed: "9"
+})
